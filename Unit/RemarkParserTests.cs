c697fc85d89ac27fb0ea098b994b84e416875835
Björn Samvik <bjorn@samvik.se>
2019-08-27 20:32:18 +0200

    Added missing file.

---
+++ Unit/RemarkParserTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrapers.Unit
+{
+    [TestFixture]
+    public class RemarkParserTests
+    {
+
+        [TestCase("Volym I. Uppslag 1-500. Kvarter 1:1-2:42.", "I")]
+        [TestCase("Volym IV. Uppslag 1501-2000. Kvarter 5:7-6:70.", "IV")]
+        [TestCase("Volym 3. Uppslag 1001-1500. Kvarter 3:34-5:30.", "3")]
+        [TestCase("Hovrättens över Skåne och Blekinge arkiv 1821-1947", "")]
+        [TestCase("", "")]
+        [TestCase(null, "")]
+
+        public void ParserTest(string remark, string volume)
+        {
+            var sourceData = new SourceData
+            {
+                Remark = remark
+            };
+
+            Assert.AreEqual(volume, sourceData.Volume);
+        }
+    }
+}
